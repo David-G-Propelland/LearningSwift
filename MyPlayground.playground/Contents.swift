48c5f64ee3f192f8fdc94229e7ef3c481aa92def
David <david@propelland.com>
2019-06-28 12:34:59 -0700

    Lesson 1 and 2
    Variables and data types

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+//VARIABLES
+//A variable holds data
+var str:String = "Hello, play area"
+
+//var(declare) name(variable name) =(asignment operator)
+print(str)
+
+var a:Int = 2 //declares the variable; variable name should be representative
+var b:Int = 1
+
+a = b //replaces data
+
+var numberOfApples = 19
+
+var c:Float = 2.3
+var d:Double = 13.9
+var e:Bool = true
+
+print(Int(c))
+print(Int(round(d)))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+//COSNTANTS
+let f = 10 // declare constants with let; this cannot be changed afterwards
