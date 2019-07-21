1009e3d32bdc0a91707258f58e5c899e9d1d487d
David <David.N.Huang@gmail.com>
2019-07-20 22:34:08 -0700

    Size variance

---
+++ Card Game Project/Card Game Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Card Game Project
+//
+//  Created by David Huang on 7/20/19.
+//  Copyright © 2019 Propelland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
