0fba8e959f9d96326802ec93e4b3dd9df9cc30a6
David <David.N.Huang@gmail.com>
2019-07-20 21:38:41 -0700

    Stacked view

---
+++ Stacked View/Stacked View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stacked View
+//
+//  Created by David Huang on 7/20/19.
+//  Copyright © 2019 Propelland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
