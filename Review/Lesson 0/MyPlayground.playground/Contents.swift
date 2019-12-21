371f36328c8776fd11a2be4cd2a89681f06e6846
David <David.N.Huang@gmail.com>
2019-12-20 18:16:05 -0800

    pushing reviews

---
+++ Review/Lesson 0/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// data is stored in memory
+// to work, we need to reference memories/data
+var firstName /*this declares firstName as a variable*/ = "Tom" /*assign the string data "Tom" to firstName*/
+
+print(firstName) // prints the data stored in firstName
+
+var stockPrice = 100
+print(stockPrice)
+
+stockPrice = 50 // here, since stockPrice is already declared, no need to redeclare it - this is assigning the new value 50
+
+// stockPrice = "Tom" // you cannot assign a different data type to one that is expecting a value type
+
+let lastName = "Smith" // this is a constant - constants cannot reassign different data
+
+
